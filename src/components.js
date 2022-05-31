422f12b43aea218eae3937060c8941ee3a96276c
Kloen Lansfiel <kloen@kloenlansfiel.com>
2022-05-31 19:03:12 +0200

    Fix pipeline.

--- src/components.js
+++ src/components.js
@@ -1,7 +1,7 @@
-import IconDiscord from "./icons/IconDiscord";
-import IconGithub from "./icons/IconGithub";
-import IconPatreon from "./icons/IconPatreon";
-import IconPaypal from "./icons/IconPaypal";
-import IconTwitter from "./icons/IconTwitter";
+import IconDiscord from "./icons/IconDiscord.vue";
+import IconGithub from "./icons/IconGithub.vue";
+import IconPatreon from "./icons/IconPatreon.vue";
+import IconPaypal from "./icons/IconPaypal.vue";
+import IconTwitter from "./icons/IconTwitter.vue";
 
 export default {IconDiscord, IconGithub, IconPatreon, IconPaypal, IconTwitter}
